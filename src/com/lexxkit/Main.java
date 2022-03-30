cff5a5837f1ed9de5a7df1c95b5f6a7ce6f9d463
Lexxkit <lexxkit@gmail.com>
2022-03-30 18:00:31 +0100

    add tasks 1 & 2

--- src/com/lexxkit/Main.java
+++ src/com/lexxkit/Main.java
@@ -3,6 +3,30 @@ package com.lexxkit;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Task 1
+        int clientOS = 0;
+
+        if (clientOS == 0) {
+            System.out.println("Install iOS application from the link.");
+        } else {
+            System.out.println("Install Android application from the link.");
+        }
+
+        // Task 2
+        int clientDeviceYear = 2013;
+        boolean isPhoneNew = clientDeviceYear >= 2015;
+
+        if (clientOS == 0 && isPhoneNew) {
+            System.out.println("Install iOS application from the link.");
+        } else if (clientOS == 0 && !isPhoneNew) {
+            System.out.println("Install the light version of iOS application from the link.");
+        }
+
+        if (clientOS == 1 && isPhoneNew) {
+            System.out.println("Install Android application from the link.");
+        } else if (clientOS == 1 && !isPhoneNew) {
+            System.out.println("Install the light version of Android application from the link.");
+        }
+
     }
 }
